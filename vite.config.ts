b3f5d27a6d6a6fd0d59032e820070292f3d3d381
StolenFought <StolenFought@gmail.com>
2025-07-09 01:23:11 +1200

    Base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/StolenThought.github.io/', // exactly like this
   plugins: [react()],
 })
